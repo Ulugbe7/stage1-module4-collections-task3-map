60a0aaf30d2ac78b17305bbde2f0d2f92eb37276
ulugbek <ulugbekyusupov272@gmail.com>
2024-02-01 20:27:45 +0500

    fix some

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -2,9 +2,10 @@ package com.epam.mjc.collections.map;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class KeyValueSwapper {
-    public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+    /*public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
 
         var newMap = new HashMap<String, Integer>();
         for (Integer i : sourceMap.keySet()) {
@@ -12,5 +13,27 @@ public class KeyValueSwapper {
         }
 
         return newMap;
+    } */
+    public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+
+        Map<String, Integer> swappedMap = new TreeMap<>(); // TreeMap to automatically order keys
+
+        for (Map.Entry<Integer, String> entry : sourceMap.entrySet()) {
+            Integer key = entry.getKey();
+            String value = entry.getValue();
+
+            // If the value is already present in the swappedMap, check for a smaller key
+            if (swappedMap.containsKey(value)) {
+                Integer existingKey = swappedMap.get(value);
+                if (key < existingKey) {
+                    swappedMap.put(value, key);
+                }
+            } else {
+                // If the value is not present, simply add the key-value pair
+                swappedMap.put(value, key);
+            }
+        }
+
+        return swappedMap;
     }
 }
